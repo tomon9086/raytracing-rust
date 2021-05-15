0e734dbe8b3229ce6eda9947115522b891495d1c
tomon9086 <tomon9086@gmail.com>
2021-05-16 02:24:17 +0900

    save as ppm

---
+++ src/image.rs
@@ -0,0 +1,38 @@
+use std::{
+  fs::{create_dir_all, File},
+  io::prelude::Write,
+  io::Result,
+  path::Path,
+};
+
+pub struct Color(pub [f64; 3]);
+
+fn create_file(path: &Path) -> Result<File> {
+  if let Some(parent) = path.parent() {
+    if let Err(why) = create_dir_all(parent) {
+      return Err(why);
+    }
+  }
+  return File::create(path);
+}
+
+pub fn save_ppm(path: &Path, width: u32, height: u32, pixels: &[Color]) -> Result<()> {
+  let mut file = create_file(path)?;
+  writeln!(file, "P3")?;
+  writeln!(file, "{} {}", width, height)?;
+  writeln!(file, "255")?;
+  for Color([r, g, b]) in pixels {
+    let f64_to_u8 = |f| (f * 255.) as u8;
+    writeln!(file, "{} {} {}", f64_to_u8(r), f64_to_u8(g), f64_to_u8(b))?;
+  }
+  file.flush()?;
+  return Ok(());
+}
+
+pub fn save(path: &Path, width: u32, height: u32, pixels: &[Color]) -> Result<String> {
+  save_ppm(path, width, height, pixels)?;
+  if let Some(path_str) = path.to_str() {
+    return Ok(path_str.to_string());
+  }
+  return Ok("".to_string());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod image;
+
+use chrono::Local;
+use image::*;
+use std::path::Path;
+
 fn main() {
-  println!("Hello, world!");
+  let (width, height): (u32, u32) = (200, 100);
+  let pixels: Vec<Color> = (0..width * height)
+    .into_iter()
+    .enumerate()
+    .map(|(i, _)| {
+      let x = (i as u32 % width) as f64;
+      let y = (i as u32 / width) as f64;
+      return Color([x / width as f64, y / height as f64, 0.5]);
+    })
+    .collect::<Vec<Color>>();
+
+  let now_str = Local::now().format("%Y-%m-%d_%H-%M-%S").to_string();
+  let filename = format!("{}.ppm", now_str);
+  let result = save(
+    Path::new(&*format!("./output/{}", filename)),
+    width,
+    height,
+    &pixels,
+  );
+  match result {
+    Ok(path) => println!("output -> {}", path),
+    Err(why) => println!("{:?}", why),
+  }
 }
