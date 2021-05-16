c35cbbb9f894a17b7addc14f65eb3ef3ff70a9be
tomon9086 <tomon9086@gmail.com>
2021-05-16 20:26:23 +0900

    x y z

--- src/rayt/float3/float3.rs
+++ src/rayt/float3/float3.rs
@@ -7,6 +7,20 @@ pub type Color = Float3;
 pub type Vec3 = Float3;
 pub type Point3 = Float3;
 
+impl Float3 {
+  pub fn x(&self) -> f64 {
+    self.to_array()[0]
+  }
+
+  pub fn y(&self) -> f64 {
+    self.to_array()[1]
+  }
+
+  pub fn z(&self) -> f64 {
+    self.to_array()[2]
+  }
+}
+
 impl Display for Float3 {
   fn fmt(&self, f: &mut Formatter<'_>) -> Result {
     let a = self.to_array();
