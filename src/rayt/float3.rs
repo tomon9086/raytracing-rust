4f81281eee50530f66502aa838b5a7449ff57c47
tomon9086 <tomon9086@gmail.com>
2021-05-16 17:21:38 +0900

    float3 from iter

--- src/rayt/float3.rs
+++ src/rayt/float3.rs
@@ -1,3 +1,5 @@
+use std::iter::FromIterator;
+
 #[derive(Debug, PartialEq)]
 pub struct Float3(pub [f64; 3]);
 
@@ -12,5 +14,22 @@ impl Float3 {
 
   pub const fn zero() -> Self {
     Self([0.; 3])
+  }
+}
+
+impl FromIterator<f64> for Float3 {
+  fn from_iter<I: IntoIterator<Item = f64>>(iter: I) -> Self {
+    let mut it = iter.into_iter();
+    let [mut x, mut y, mut z] = [0.; 3];
+    if let Some(nx) = it.next() {
+      x = nx;
+    }
+    if let Some(ny) = it.next() {
+      y = ny;
+    }
+    if let Some(nz) = it.next() {
+      z = nz;
+    }
+    Self::new(x, y, z)
   }
 }
