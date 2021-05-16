8568bb2cb47f74867ac521306e6be303ded75dff
tomon9086 <tomon9086@gmail.com>
2021-05-16 19:26:45 +0900

    impl Display for Float3

--- src/rayt/float3/float3.rs
+++ src/rayt/float3/float3.rs
@@ -1,6 +1,15 @@
+use std::fmt::{Display, Formatter, Result};
+
 #[derive(Debug, PartialEq)]
 pub struct Float3(pub [f64; 3]);
 
 pub type Color = Float3;
 pub type Vec3 = Float3;
 pub type Point3 = Float3;
+
+impl Display for Float3 {
+  fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+    let a = self.to_array();
+    write!(f, "({}, {}, {})", a[0], a[1], a[2])
+  }
+}
