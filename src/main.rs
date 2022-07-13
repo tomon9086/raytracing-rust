931c19fc114f246429f4feb7638dda91b8951e9d
tomon9086 <tomon9086@gmail.com>
2022-07-13 13:42:56 +0900

    rgb

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use image::{codecs::png::PngEncoder, ColorType, ImageEncoder};
+use rgb::RGB;
 use std::{fs, io, path};
+
+type Color = RGB<u8>;
 
 fn main() {
     let bounds = (500, 500);
-    let mut image_buffer = vec![0; bounds.0 * bounds.1 * 3]
+    let mut image_buffer = vec![0; bounds.0 * bounds.1]
         .iter()
         .enumerate()
         .map(|(i, _p)| {
-            let x = (i / 3) % bounds.0;
-            let y = (i / 3) / bounds.0;
+            let x = i % bounds.0;
+            let y = i / bounds.0;
             let u = x as f32 / bounds.0 as f32;
             let v = y as f32 / bounds.1 as f32;
-            let rgb = i % 3;
 
-            if rgb == 0 {
-                (u * 255.) as u8
-            } else if rgb == 1 {
-                (v * 255.) as u8
-            } else {
-                128
+            Color {
+                r: (u * 255.) as u8,
+                g: (v * 255.) as u8,
+                b: 128,
             }
         })
-        .collect::<Vec<u8>>();
+        .collect::<Vec<Color>>();
 
     save_image(
         &format!(
@@ -34,7 +34,7 @@ fn main() {
     .expect("error writing image");
 }
 
-fn save_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), io::Error> {
+fn save_image(filename: &str, pixels: &[Color], bounds: (usize, usize)) -> Result<(), io::Error> {
     path::Path::new(filename).parent().and_then(|p| {
         if !p.exists() {
             let _ = fs::create_dir_all(p);
@@ -45,7 +45,15 @@ fn save_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(
     let file = fs::File::create(filename)?;
 
     let encoder = PngEncoder::new(file);
-    let _ = encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Rgb8);
+    let _ = encoder.write_image(
+        &pixels
+            .iter()
+            .flat_map(|p| [p.r, p.g, p.b])
+            .collect::<Vec<u8>>(),
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Rgb8,
+    );
 
     Ok(())
 }
